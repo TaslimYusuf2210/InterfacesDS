10ecae8d194d15ee1f37a0ba378235af61f96918
Taslim Yusuf <Omotoyosi2210@gmail.com>
2025-11-15 17:23:58 +0100

    Navbar 95%

--- Desktop/InterfacesDS/interfacesds/src/components/GetandPreview.jsx
+++ Desktop/InterfacesDS/interfacesds/src/components/GetandPreview.jsx
@@ -1,6 +1,6 @@
-function GetandPreview() {
+function GetandPreview({nav=true}) {
     return ( 
-        <div className="flex gap-2">
+        <div className={`flex gap-2 ${ nav ? "flex-row": "flex-row-reverse p-2"}`}>
                     <button className="cursor-pointer text-black font-medium bg-white rounded-md py-3 px-4 hover:bg-blue-50">Get it now</button>
                     <button className="cursor-pointer border font-medium border-accent px-6 py-3 rounded-md hover:text-black hover:bg-gray-100">Preview</button>
                 </div>
--- Desktop/InterfacesDS/interfacesds/src/components/Navbar.jsx
+++ Desktop/InterfacesDS/interfacesds/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import GetandPreview from "./GetandPreview";
 
 function Navbar() {
     return ( 
-        <nav className="bg-black flex justify-between px-4 pt-3 pb-3 border-b-accent border-b fixed w-full z-9999">
+        <nav className="bg-black flex justify-between px-4 pt-2 pb-2 border-b-accent border-b fixed w-full z-9999">
             <div className="flex items-center gap-2">
                 <span className="text-4xl">
                     <SiCssdesignawards />
@@ -14,23 +14,31 @@ function Navbar() {
                 </span>
             </div>
             <div className="flex items-center gap-4 font-medium">
-                <div className="flex gap-12">
+                <div className="flex items-center gap-3">
                     <NavLink
                     to="/"
                     end
-                    className={({isActive}) => isActive ? "text-red" : ""}
+                    className={({isActive}) => 
+                    `hover:bg-accent rounded-sm px-5 flex flex-col items-center gap-2 py-3 h-fit
+                    ${isActive ? " pb-0 after:content-[''] after:w-16 after:block after:h-0.5 after:bg-white " : ""} `}
                     >Home</NavLink>
                     <NavLink
                     to="/pricing"
-                    className={({isActive}) => isActive ? "text-red" : ""}
+                    className={({isActive}) => 
+                    `hover:bg-accent rounded-sm px-5 flex flex-col items-center gap-2 py-3 
+                    ${isActive ? "pb-0 after:content-[''] after:w-16 after:block after:h-0.5 after:bg-white " : ""} `}
                     >Pricings</NavLink>
                     <NavLink
                     to="/articles"
-                    className={({isActive}) => isActive ? "text-red" : ""}
+                    className={({isActive}) => 
+                    `hover:bg-accent rounded-sm px-5 flex flex-col items-center gap-2 py-3 
+                    ${isActive ? "pb-0 after:content-[''] after:w-16 after:block after:h-0.5 after:bg-white " : ""} `}
                     >Articles</NavLink>
                     <NavLink>Contact</NavLink>
                 </div>
-                <GetandPreview></GetandPreview>
+                <GetandPreview
+                nav={false}
+                ></GetandPreview>
             </div>
         </nav>
      );
